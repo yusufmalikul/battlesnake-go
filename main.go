68abe805543e4a134199da73e8937cd10efefa43
yusufmalikul <yusuflinuxer@gmail.com>
2023-12-29 15:19:13 +0700

    use flood fill algorithm to avoid enclosed spaces

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ package main
 // For more info see docs.battlesnake.com
 
 import (
+	"fmt"
 	"log"
-	"math/rand"
 )
 
 // info is called when you create your Battlesnake on play.battlesnake.com
@@ -95,16 +95,10 @@ func move(state GameState) BattlesnakeMoveResponse {
 	log.Println("myHead.X", myHead.X, "boardWidth", boardWidth)
 
 	// Step 2 - Prevent your Battlesnake from colliding with itself
-	safeMoves := []string{}
-	for move, isSafe := range isMoveSafe {
-		if isSafe {
-			safeMoves = append(safeMoves, move)
-		}
-	}
-	log.Println("start debug body")
+	// log.Println("start debug body")
 	mybody := state.You.Body
 	for _, v := range mybody {
-		log.Println("body X:", v.X, "body Y:", v.Y)
+		// log.Println("body X:", v.X, "body Y:", v.Y)
 		if isMoveSafe["up"] && myHead.Y+1 == v.Y && myHead.X == v.X {
 			isMoveSafe["up"] = false
 		}
@@ -121,13 +115,66 @@ func move(state GameState) BattlesnakeMoveResponse {
 			isMoveSafe["left"] = false
 		}
 	}
-	log.Println("end debug body")
+	// log.Println("end debug body")
 
-	// TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-	// opponents := state.Board.Snakes
+	// Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
+	opponents := state.Board.Snakes
+	for _, v := range opponents {
+		for _, snake := range v.Body {
+			// log.Println("body X:", snake.X, "body Y:", snake.Y)
+			if isMoveSafe["up"] && myHead.Y+1 == snake.Y && myHead.X == snake.X {
+				isMoveSafe["up"] = false
+			}
+
+			if isMoveSafe["down"] && myHead.Y-1 == snake.Y && myHead.X == snake.X {
+				isMoveSafe["down"] = false
+			}
+
+			if isMoveSafe["right"] && myHead.X+1 == snake.X && myHead.Y == snake.Y {
+				isMoveSafe["right"] = false
+			}
+
+			if isMoveSafe["left"] && myHead.X-1 == snake.X && myHead.Y == snake.Y {
+				isMoveSafe["left"] = false
+			}
+		}
+	}
+
+	// Prevent entering closed grid with no way to escape
+	// we will use flood fill algorithm
+	// First we mark all filled grid (filled by our snake body and other snake body)
+	moveSafeGridSize := map[string]int{
+		"up":    0,
+		"down":  0,
+		"left":  0,
+		"right": 0,
+	}
+
+	if isMoveSafe["up"] {
+		total := fill(prepareGrid(boardWidth+1, boardHeight+1, opponents), boardWidth, boardHeight, myHead.X, myHead.Y+1, "", "@")
+		moveSafeGridSize["up"] = total
+	}
+
+	if isMoveSafe["down"] {
+		total := fill(prepareGrid(boardWidth+1, boardHeight+1, opponents), boardWidth, boardHeight, myHead.X, myHead.Y-1, "", "@")
+		moveSafeGridSize["down"] = total
+	}
+
+	if isMoveSafe["right"] {
+		total := fill(prepareGrid(boardWidth+1, boardHeight+1, opponents), boardWidth, boardHeight, myHead.X+1, myHead.Y, "", "@")
+		moveSafeGridSize["right"] = total
+	}
+
+	if isMoveSafe["left"] {
+		total := fill(prepareGrid(boardWidth+1, boardHeight+1, opponents), boardWidth, boardHeight, myHead.X-1, myHead.Y, "", "@")
+		moveSafeGridSize["left"] = total
+	}
+
+	fmt.Println("isMoveSafe", isMoveSafe)
+	fmt.Println("moveSafeGridSize", moveSafeGridSize)
 
 	// Are there any safe moves left?
-	safeMoves = []string{}
+	safeMoves := []string{}
 	for move, isSafe := range isMoveSafe {
 		if isSafe {
 			safeMoves = append(safeMoves, move)
@@ -141,13 +188,73 @@ func move(state GameState) BattlesnakeMoveResponse {
 	}
 
 	// Choose a random move from the safe ones
-	nextMove := safeMoves[rand.Intn(len(safeMoves))]
+	// nextMove := safeMoves[rand.Intn(len(safeMoves))]
+
+	// Choose best move with largest room
+	nextMove := "up"
+	largestRoom := moveSafeGridSize["up"]
+	for i := range moveSafeGridSize {
+		if moveSafeGridSize[i] > largestRoom {
+			largestRoom = moveSafeGridSize[i]
+			nextMove = i
+		}
+	}
 
 	// TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
 	// food := state.Board.Food
 
 	log.Printf("MOVE %d: %s\n", state.Turn, nextMove)
 	return BattlesnakeMoveResponse{Move: nextMove}
+}
+
+func fill(grid [][]string, boardWidth, boardHeight, x int, y int, old string, new string) int {
+
+	total := 0
+
+	if x < 0 {
+		return 0
+	}
+
+	if y < 0 {
+		return 0
+	}
+
+	if x > boardWidth {
+		return 0
+	}
+
+	if y > boardHeight {
+		return 0
+	}
+
+	if grid[x][y] == new || grid[x][y] != old {
+		return 0
+	}
+
+	grid[x][y] = "X"
+	total++
+
+	total += fill(grid, boardWidth, boardHeight, x+1, y, old, new)
+	total += fill(grid, boardWidth, boardHeight, x-1, y, old, new)
+	total += fill(grid, boardWidth, boardHeight, x, y+1, old, new)
+	total += fill(grid, boardWidth, boardHeight, x, y-1, old, new)
+
+	return total
+}
+
+func prepareGrid(width int, height int, opponents []Battlesnake) [][]string {
+	markedGrid := make([][]string, width)
+	for i := range markedGrid {
+		markedGrid[i] = make([]string, height)
+	}
+	for _, v := range opponents {
+		// fmt.Println("debug grid")
+		for _, snake := range v.Body {
+			// fmt.Println("X", snake.X, "Y", snake.Y)
+			markedGrid[snake.X][snake.Y] = "X"
+		}
+	}
+	return markedGrid
 }
 
 func main() {
